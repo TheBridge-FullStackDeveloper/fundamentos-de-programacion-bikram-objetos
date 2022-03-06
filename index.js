cc0ec69554b4d9df026df7cb16edbae511afb63f
Brujitari <natimontesg2@mail.com>
2022-03-06 01:21:32 +0100

    feat: ejercicios 1 y 2

---
+++ index.js
@@ -0,0 +1,16 @@
+1// 
+const Coche = {
+    marca: 'marca',
+    modelo: 'modelo',
+    matricula: 'matricula',
+};
+
+//2
+const Casa = {
+    codPostal: 00000,
+    calle: 'calle',
+    portal: 00,
+    piso: 0,
+};
+
+//3
